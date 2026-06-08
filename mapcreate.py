d6a26170fb2c161380ac4249ebc96ca9b86453cb
GARDMAN-001 <65041308125@udru.ac.th>
2026-06-08 13:54:22 +0700

    Update mapcreate.py

--- mapcreate.py
+++ mapcreate.py
@@ -278,10 +278,10 @@ class OdomRobot:
             (1, 2):  [("move", 10.0, r), ("move", 5.2, z), ("rotate", 90), ("move", 1.0)],
             (1, 3):  [("move", 10.0, r), ("move", 10.0, z), ("move", 7.2, r), ("rotate", 90), ("move", 1.0)],
            
-            (2, 1):  [("rotate", 180), ("move", 1.0), ("rotate", -90), ("move", 10.0, l), ("move", 5.2, z), ("rotate", -90), ("move", 6.5), ("rotate", -90)],
+            (2, 1):  [("rotate", 180), ("move", 1.0), ("rotate", -90), ("move", 10.0, l), ("move", 5.2, z)],
             (2, 3):  [("rotate", 180), ("move", 1.0), ("rotate", 90), ("move", 10.0, l), ("move", 2.5, r), ("rotate", 90), ("move", 1.0)],
            
-            (3, 1):  [("rotate", 180), ("move", 1.0), ("rotate", -90), ("move", 10.0, r), ("move", 10.0, r), ("move", 7.2, r), ("rotate", -90), ("move", 6.5), ("rotate", -90)],
+            (3, 1):  [("rotate", 180), ("move", 1.0), ("rotate", -90), ("move", 10.0, r), ("move", 10.0, r), ("move", 7.2, r)],
             (3, 2):  [("rotate", 180), ("move", 1.0), ("rotate", -90), ("move", 10.0, r), ("move", 2.5, r), ("rotate", -90), ("move", 1.0)],
 
         }
